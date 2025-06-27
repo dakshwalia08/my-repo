72c050964b8250d65276dc85cf7c3e4bff8d445a
dakshwalia08 <dakshwaliaddn@gmail.com>
2025-06-27 21:34:22 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,24 +1,10 @@
 #include <stdio.h>
 int main (){
-    int flag=0;
-    int n;
-    scanf("%d",&n);
-    if (n==1||n==0){
-        flag=1;
-        
+    int product=1;
+    int n=4;
+    for(int i=1;i<=n;i++){
+        product*=i;
     }
-    else{
-        for(int i=2;i<n;i++){
-            if(n%i==0){
-                flag=1;
-            }
-        }
-    }
-    if(flag){
-        printf("the number is not prime");
-    }
-    else{
-        printf("the number is prime");
-    }
+    printf("the factorial of number is %d",product);
     return 0;
 }
